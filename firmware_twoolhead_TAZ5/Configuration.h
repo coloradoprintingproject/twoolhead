462cd3dddbc320549206adcd0cb8691f16188d2f
Brent (bam) <bam@coloradoprintingproject.org>
2017-10-05 05:34:10 -0600

    stepper current updated to OMC stepper value in both firmwares

--- firmware_twoolhead_TAZ5/Configuration.h
+++ firmware_twoolhead_TAZ5/Configuration.h
@@ -349,7 +349,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 // ENDSTOP SETTINGS:
 // Sets direction of endstops when homing; 1=MAX, -1=MIN
 #define X_HOME_DIR -1
-#define Y_HOME_DIR 1
+#define Y_HOME_DIR -1
 #define Z_HOME_DIR -1
 
 #define min_software_endstops true // If true, axis won't move to coordinates less than HOME_POS.
@@ -358,7 +358,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 // Travel limits after homing
 #define X_MAX_POS 150
 #define X_MIN_POS 0
-#define Y_MAX_POS 303
+#define Y_MAX_POS 300
 #define Y_MIN_POS 0
 #define Z_MAX_POS 250
 #define Z_MIN_POS 0
@@ -454,8 +454,8 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
   #ifdef Z_SAFE_HOMING
 
-    #define Z_SAFE_HOMING_X_POINT (-19)    // X point for Z homing when homing all axis (G28)
-    #define Z_SAFE_HOMING_Y_POINT (258)    // Y point for Z homing when homing all axis (G28)
+    #define Z_SAFE_HOMING_X_POINT (10)    // X point for Z homing when homing all axis (G28)
+    #define Z_SAFE_HOMING_Y_POINT (10)    // Y point for Z homing when homing all axis (G28)
 
   #endif
 
